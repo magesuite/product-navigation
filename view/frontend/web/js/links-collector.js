90f96fbf05e4310d8341df9f2e9ba3aba68e2ecd
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2020-09-23 13:36:53 +0200

    Improve links collecting performance

--- view/frontend/web/js/links-collector.js
+++ view/frontend/web/js/links-collector.js
@@ -1,4 +1,4 @@
-define(['jquery', 'jquery-ui-modules/widget'], function($) {
+define(['jquery', 'underscore', 'jquery-ui-modules/widget'], function ($, _) {
     'use strict';
 
     $.widget('magesuite.linksCollector', {
@@ -6,23 +6,23 @@ define(['jquery', 'jquery-ui-modules/widget'], function($) {
             productLinkSelector: '.cs-product-tile__name-link',
             storageKey: 'mgs-product-navigation',
         },
-        _create: function() {
+        _create: function () {
             var currentUrl = window.location.href;
-
             this._saveCategoryInfo({
                 url: currentUrl,
                 products: this._collectLinks(),
             });
         },
-        _collectLinks: function() {
-            return this.element
-                .find(this.options.productLinkSelector)
-                .map(function() {
-                    return $(this).attr('href');
-                })
-                .get();
+        _collectLinks: function () {
+            return _.toArray(
+                this.element[0].querySelectorAll(
+                    this.options.productLinkSelector
+                )
+            ).map(function (productLink) {
+                return productLink.getAttribute('href');
+            });
         },
-        _saveCategoryInfo: function(categoryInfo) {
+        _saveCategoryInfo: function (categoryInfo) {
             localStorage.setItem(
                 this.options.storageKey,
                 JSON.stringify(categoryInfo)
